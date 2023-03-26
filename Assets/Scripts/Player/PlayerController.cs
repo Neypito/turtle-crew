67b850abdb27fc1dc7d8d9ccb229c56a7442921f
Neypito <60406965+Neypito@users.noreply.github.com>
2023-03-26 20:55:40 +0200

    Physics 26.02.2023
    Jump and controls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,5 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+    private bool facingRight = true;
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     
+    
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if(facingRight == false && moveInput > 0) 
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGrounded);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
 }
